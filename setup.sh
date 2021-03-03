e524d9a01867e373e9e91a4dcc844e1f246805f0
pavel.sheinfeld <pavel.sheinfeld@gmail.com>
2021-03-03 18:51:09 +0000

    v1.1

--- setup.sh
+++ setup.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 echo -e "\e[1;32m => provisioning ALL resources \e[0m"
-sh ./provision-compartment.sh
-sh ./provision-network.sh
-sh ./provision-resources.sh
-sh ./provision-customer-agreement.sh
-sh ./provision-compute.sh
+sh $SCRIPTPATH/provision-compartment.sh
+sh $SCRIPTPATH/provision-network.sh
+sh $SCRIPTPATH/provision-resources.sh
+sh $SCRIPTPATH/provision-customer-agreement.sh
+sh $SCRIPTPATH/provision-compute.sh
+
+
+echo -e "\e[1;32m <= DONE \e[0m"
+dt=$(cat $SCRIPTPATH/instance_public_ip)
+echo -e "\e[1;32m SSH to : $dt \e[0m"
+dt=$(cat $SCRIPTPATH/object_storage_preauth_link)
+echo -e "\e[1;32m SSH key : $dt \e[0m"
+
+
 
